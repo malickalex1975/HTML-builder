957871579870ae40f19769c01f427dceaf811db5
Malchevsky Alexander <malickalex1975@gmail.com>
2022-05-16 16:38:14 +0300

    feat: add 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
 
+
+
+const fsPromises = require('fs/promises');
+const path = require('path');
+const source = path.join(__dirname, 'files');
+const destination = path.join(__dirname, 'files-copy');
+
+async function copyDir(){
+  await fsPromises.mkdir(destination,{recursive:true});
+
+  const files=  await fsPromises.readdir(source);
+  for(let file of files){
+    await fsPromises.copyFile(path.join(source,file),path.join(destination,file));
+  }
+}
+copyDir();
